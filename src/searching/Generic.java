d018957124bc0098d7f3aa3b66c6c9dcab486a2f
priyanka <priyanka@gmail.com>
2023-02-09 15:44:10 +0530

    generic class created

---
+++ src/searching/Generic.java
@@ -0,0 +1,22 @@
+package searching;
+
+public class Generic {
+public static void main(String[] args) {
+	
+}
+
+public static void main2(String[] args) {
+	
+}
+
+
+public static void main3(String[] args) {
+	
+}
+
+
+public static void main4(String[] args) {
+	
+}
+
+}
