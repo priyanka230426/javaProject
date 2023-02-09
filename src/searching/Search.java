c5019845dd69a32fb53c5252be8e8307ffaabbaa
priyanka <priyanka@gmail.com>
2023-02-09 15:59:54 +0530

    m1,m2 ,m3 ,4 is created

--- src/searching/Search.java
+++ src/searching/Search.java
@@ -9,4 +9,16 @@ public class Search {
 	public static void main1(String[] args) {
 
 	}
+	
+	public static void main2(String[] args) {
+
+	}
+	
+	public static void main3(String[] args) {
+
+	}
+	
+	public static void main4(String[] args) {
+
+	}
 }
