0c9f810b45d5808ac7c527806601b7b275df2af3
priyanka <priyanka@gmail.com>
2023-02-11 15:25:01 +0530

    all project java

--- src/google/search/Searching.java
+++ src/google/search/Searching.java
@@ -2,4 +2,12 @@ package google.search;
 
 public class Searching {
 
+	  public static void m1() {
+		  System.out.println("This is m1 method");
+	  }
+	
+	  public static void m2() {
+		  System.out.println("This is m2 method");
+	  }	
+	
 }
